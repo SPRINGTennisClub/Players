d9df89d83e6f36a252810a7846c7536da5e1c1c3
aubertMal <malek.aubert@gmail.com>
2021-07-21 09:47:09 +0200

    feat: ajout service de gestion des joueurs

---
+++ src/test/java/com/aubert/players/PlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aubert.players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
